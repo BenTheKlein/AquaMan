96e6521d3108cffc8cd101ee0d50a833edb9ebba
Ben <>
2023-01-20 17:24:04 +0200

    asdf

--- Server/simple_server.py
+++ Server/simple_server.py
@@ -24,7 +24,8 @@ class myHandler(BaseHTTPRequestHandler):
 		try:
 			if 'health' in self.path:
 				self.send_response(200)
-				self.send_header('Content-type',mimetype)
+				self.send_header('Content-type','text/html')
+				self.wfile.write('OK')
 				self.end_headers()
 				return
 
